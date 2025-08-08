463be73cf04e238546e1d059dbb0f29dccf8b5b5
Danilo <daniloromao.sw@gmail.com>
2025-08-08 16:12:48 -0300

    Feet: manipulation error handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import fastify from 'fastify'
 import SetupRoutes from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const server = fastify()
 
 server.register(SetupRoutes)
+
+server.setErrorHandler((error, _, reply) => {
+   if (error instanceof ZodError) {
+      return reply.status(400).send({
+         message: 'Validation error',
+         issues: error.issues,
+      })
+   }
+
+   if (env.NODE_ENV !== 'prod') {
+      console.error(error)
+   }
+
+   return reply.status(500).send({
+      message: 'Internal server error',
+      error: error.message,
+   })
+})
