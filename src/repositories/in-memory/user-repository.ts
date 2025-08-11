e3b19d8779cdb4e8350c35cb8be36124503b6d86
Danilo Romão <https.sw@gmail.com>
2025-08-11 07:47:16 -0300

    Feat: implement test

---
+++ src/repositories/in-memory/user-repository.ts
@@ -0,0 +1,26 @@
+import type { IUserRepository } from '../user-repositoy'
+import { type Prisma, type User } from '@prisma/client'
+
+export class InMememoryUserRepository implements IUserRepository {
+   public itens: User[] = []
+
+   async findByEmail(email: string): Promise<User | null> {
+      const user = this.itens.find((user) => user.email === email)
+
+      return user ? user : null
+   }
+
+   async create(data: Prisma.UserCreateInput): Promise<User> {
+      const user = {
+         id: 'user-id-1',
+         name: data.name,
+         email: data.email,
+         password_hash: data.password_hash,
+         created_at: new Date(),
+      }
+
+      this.itens.push(user)
+
+      return user
+   }
+}
---
+++ src/test/services/register.spec.ts
@@ -0,0 +1,56 @@
+import { UserAlreadyExistsError } from '@/_errors/user-alredy-exists'
+import { InMememoryUserRepository } from '@/repositories/in-memory/user-repository'
+import { RegisterService } from '@/service/register'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+
+describe('Register Services', () => {
+   it('should be able to register ', async () => {
+      const repository = new InMememoryUserRepository()
+      const service = new RegisterService(repository)
+
+      const { user } = await service.execute({
+         name: 'John Doe',
+         email: 'jhon@exemple.com',
+         password: '123456',
+      })
+
+      expect(user.id).toEqual(expect.any(String))
+   })
+
+   it('should hash user password upon registration', async () => {
+      const repository = new InMememoryUserRepository()
+      const service = new RegisterService(repository)
+
+      const { user } = await service.execute({
+         name: 'John Doe',
+         email: 'jhon@exemple.com',
+         password: '123456',
+      })
+
+      const isPassawordCorrectly = await compare('123456', user.password_hash)
+
+      expect(isPassawordCorrectly).toBe(true)
+   })
+
+   it('should not be able to register with same email twice', async () => {
+      const repository = new InMememoryUserRepository()
+      const service = new RegisterService(repository)
+
+      const email = 'jhon@exemple.com'
+
+      await service.execute({
+         name: 'John Doe',
+         email,
+         password: '123456',
+      })
+
+      expect(() =>
+         service.execute({
+            name: 'John Doe',
+            email,
+            password: '123456',
+         }),
+      ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+   })
+})
