cb641d0d2dca17d1edaf0c5f8b5434225a804127
Danilo Romão <https.sw@gmail.com>
2026-01-26 09:22:15 -0300

    Feat: create environment test workspace

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,25 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
    plugins: [tsConfigPaths()],
+   test: {
+      dir: 'src',
+      projects: [
+         {
+            extends: true,
+            test: {
+               name: 'unit',
+               dir: 'src/services',
+            },
+         },
+         {
+            extends: true,
+            test: {
+               name: 'e2e',
+               dir: 'src/http/controllers',
+               environment:
+                  './prisma/vitest-environment-prisma/prisma-test-environment',
+            },
+         },
+      ],
+   },
 })
