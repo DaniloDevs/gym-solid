47db4c77df912c713842652c01ede012788c7edf
Danilo <htpps.sw@gmail.com>
2026-01-28 17:14:18 -0300

    Feat: create profile test e2e

---
+++ src/http/controllers/test/profile.spec.ts
@@ -0,0 +1,38 @@
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { server } from '@/index'
+
+describe('Profile (e2e)', () => {
+    beforeAll(async () => {
+        await server.ready()
+    })
+
+    afterAll(async () => {
+        await server.close()
+    })
+
+    test('should be able to get user profile', async () => {
+        await request(server.server).post('/users').send({
+            name: 'Jhon Doe',
+            email: 'teste@gmail.com',
+            password: '123456',
+        })
+
+        const authResponse = await request(server.server).post('/sessions').send({
+            email: 'teste@gmail.com',
+            password: '123456',
+        })
+
+        const { token } = authResponse.body
+
+        const profileResponse = await request(server.server)
+            .get('/me')
+            .set('Authorization', `Bearer ${token}`)
+            .send()
+
+        expect(profileResponse.statusCode).toEqual(200)
+        expect(profileResponse.body.user).toEqual(expect.objectContaining({
+            email: 'teste@gmail.com'
+        }))
+    })
+})
