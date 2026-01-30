acec8b807aef5d0845ff89c2582520df24ef516f
Danilo <daniloromao.sw@gmail.com>
2026-01-30 10:32:50 -0300

    fix

--- src/http/controllers/gym/nearby.ts
+++ src/http/controllers/gym/nearby.ts
@@ -2,7 +2,10 @@ import { MakeFetchNearbyGymsService } from '@/services/factories/make-fetch-near
 import { type FastifyReply, type FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-export async function nearbyController(request: FastifyRequest, reply: FastifyReply) {
+export async function nearbyController(
+   request: FastifyRequest,
+   reply: FastifyReply,
+) {
    const nearbyGymsQuerySchema = z.object({
       latitude: z.number().refine((value) => {
          return Math.abs(value) <= 90
